f8208e2693a304ecda03030c73645934bbbd9e22
Aryan Zutshi <122102323+Aryanzutshi@users.noreply.github.com>
2024-10-13 12:31:39 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <TypewriterEffectSmoothDemo/>
      <SparklesPreview/> 
      <TextGenerateEffectDemo/> 
-     <BackgroundGradientDemo/> 
+      {/* <BackgroundGradientDemo/> */} 
      <HeroParallaxDemo/>
     <TracingBeamDemo/>
     <GoogleGeminiEffectDemo/>
