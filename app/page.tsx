d696e65405502a3271c13535a7f3b481d30c2c59
Aryan Zutshi <122102323+Aryanzutshi@users.noreply.github.com>
2025-02-18 20:43:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import { ThreeDCardDemo } from "@/components/3DCard";
 import { TracingBeamDemo } from "@/components/Beam";
-import { BackgroundGradientDemo } from "@/components/Cards";
+import { ThreeDCardDemo } from "@/components/3DCard";
 import { GoogleGeminiEffectDemo } from "@/components/GoogleGemini";
+import { BackgroundGradientDemo } from "@/components/Cards";
 import { HeroParallaxDemo } from "@/components/Hero";
 import { FloatingNavDemo } from "@/components/Nav";
-// import { NavbarDemo } from "@/components/Nav";
-import { SparklesPreview } from "@/components/SparkleText";
+import { NavbarDemo } from "@/components/Nav";
 import { TextGenerateEffectDemo } from "@/components/TextGenerate";
-import { TypewriterEffectSmoothDemo } from "@/components/Typo";
+import { SparklesPreview } from "@/components/SparkleText";
 import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { TypewriterEffectSmoothDemo } from "@/components/Typo";
 import Image from "next/image";
 
 export default function Home() {
@@ -19,7 +19,7 @@ export default function Home() {
     <TypewriterEffectSmoothDemo/>
      <SparklesPreview/> 
      <TextGenerateEffectDemo/> 
-      {/* <BackgroundGradientDemo/> */} 
+      <BackgroundGradientDemo/> 
      <HeroParallaxDemo/>
     <TracingBeamDemo/>
     <GoogleGeminiEffectDemo/>
